37fb94dd249e82db7d02a5b79d7805735b907ca3
Quentin Ågren <quentin@scilifelab.se>
2024-03-07 13:35:47 +0100

    Add local BAM alignments to clu har

--- data/clupea_harrengus/scripts.sh
+++ data/clupea_harrengus/scripts.sh
@@ -12,3 +12,8 @@ add_connection() {
     url="https://hgdownload.soe.ucsc.edu/hubs/GCF/900/700/415/GCF_900700415.2/hub.txt"
     jbrowse add-connection --name="Clupea Harengus (UCSC)" --assemblyNames="GCF_900700415.2" $url
 }
+
+add_local_bam_track() {
+    BAM="GCF_900700415.2/GCF_900700415.2_Ch_v2.0.2_knownrefseq_alns.bam"
+    jbrowse add-track "$BAM" --load=inPlace --description="RefSeq BAM alignment track" --category="Local tracks"
+}
