3c7e04cfd0cd1b6f5353e0db581dbc3caca3a49a
Quentin Ågren <quentin@scilifelab.se>
2024-03-27 10:45:18 +0100

    Expect bgzipped GFF files for indexing

--- scripts/index_gff.sh
+++ scripts/index_gff.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 shopt -s extglob
+
+# Usage: index_gff FILENAME
+#
+#   FILENAME is expected to be a block-gzipped GFF file
+#   with extension .gff.gz
+
+
 GFF="$1"
-GFF_SORTED="${GFF/.gff?(.?(b)gz)/.sorted.gff.bgz}"
+SORTED="${GFF/.gff.bgz/.sorted.gff.bgz}"
+INDEX="${GFF}.tbi"
 
-echo "Sorting, compressing and indexing $GFF into ${GFF_SORTED}"
 
-zcat "$GFF" | grep -v "^#" | sort -t$'\t' -k1,1 -k4,4n | bgzip > "$GFF_SORTED"
+echo "Creating index ${INDEX} for $GFF"
 
-tabix -p gff "$GFF_SORTED"
+zcat "$GFF" | grep -v "^#" | sort -t$'\t' -k1,1 -k4,4n | bgzip > "$SORTED"
+
+tabix -p gff "$SORTED"
+mv "${SORTED}.tbi" "${INDEX}"
+rm "$SORTED"
 
