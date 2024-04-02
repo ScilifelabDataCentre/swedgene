91ae1c817ea01b69c53bdd749f27c1980fb3d229
Quentin Ågren <quentin@scilifelab.se>
2024-04-02 15:23:19 +0200

    Generate tracks configuration

--- scripts/generate_jbrowse_config.sh
+++ scripts/generate_jbrowse_config.sh
@@ -2,9 +2,10 @@
 
 YAML="$1"
 DIR="$(dirname $YAML)"
+TARGET="$DIR/config.json"
 
 add_assemblies() {
-    while read name url aliases; do
+    while IFS=';' read -r url name aliases; do
 	LOCAL_FILE="$DIR/$(basename $url)"
 	# Block-gzipped files are saved locally with the explicit .bgz extension
 	if [[ "$LOCAL_FILE" =~ fna.gz$ ]]; then
@@ -15,12 +16,30 @@ add_assemblies() {
 	fi
 	if [[ -e ${LOCAL_FILE} ]]; then
 	    echo "Using local file: $LOCAL_FILE"
-	    url="$LOCAL_FILE"
+	    url="--load=inPlace $LOCAL_FILE"
 	fi
-	jbrowse add-assembly --force --type="$TYPE" --load=inPlace --target="$DIR/config.json" --name="$name" --refNameAliases="$aliases" "$url"
+	jbrowse add-assembly --force --type="$TYPE" --target="$TARGET" --name="$name" --refNameAliases="$aliases" "$url"
     done
 }
 
-yq '.assembly | [.name, .url, .aliases] | join(" ")' "$YAML" | add_assemblies
+add_tracks () {
+    while IFS=';' read -r url name; do
+	LOCAL_FILE="$DIR/$(basename $url)"
+	if [[ "$LOCAL_FILE" =~ gff.gz$ ]]; then
+	    LOCAL_FILE="${LOCAL_FILE/.gz/.bgz}"
+	fi
+	if [[ -e ${LOCAL_FILE} ]]; then
+	    echo "Using local file: $LOCAL_FILE"
+	    url="--load=inPlace $LOCAL_FILE"
+	fi
+	jbrowse add-track --force --name="$name" --target="$TARGET" $url
+    done
+}
 
+generate_assembly_config () {
+    yq '.assembly | [.url, .name, .aliases] | join(";")' "$YAML" | add_assemblies
+}
 
+generate_tracks_config() {
+    yq '.tracks[] | [.url, .name] | join(";")' "$YAML" | add_tracks
+}
