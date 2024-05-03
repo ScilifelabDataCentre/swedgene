a37f06f8724c5ba6f1ba8ce0de6e745460385c6c
Daniel P Brink <daniel.brink@scilifelab.se>
2024-05-03 09:15:41 +0200

    Style fixes to comply with ruff and pre-commit

--- scripts/get_aliases_from_ENA_fasta.py
+++ scripts/get_aliases_from_ENA_fasta.py
@@ -1,7 +1,7 @@
 """
-This script generates an JBrowse2-compatible refNameAliases file from an ENA fasta file. The alias file will 
-allow for displaying annotation tracks from NCBI GenBank on an ENA formatted assembly without having to modify 
-the data files themselves. Additionally, for cases where the original contig name is preserved in the ENA fasta 
+This script generates an JBrowse2-compatible refNameAliases file from an ENA fasta file. The alias file will
+allow for displaying annotation tracks from NCBI GenBank on an ENA formatted assembly without having to modify
+the data files themselves. Additionally, for cases where the original contig name is preserved in the ENA fasta
 header, it will also allow for displaying tracks that calls on contig name fasta headers.
 
 Example of a ENA formatted fasta header from a genome assembly:
@@ -18,90 +18,91 @@ python get_aliases_from_ENA_fasta.py --fasta filename.fa
 
 """
 
-import sys
 import argparse
 import re
 
 
 def parse_the_arguments():
-	"""
-	Run argparse to set up and capture the arguments of the script.
-	"""
+    """
+    Run argparse to set up and capture the arguments of the script.
+    """
 
-	parser = argparse.ArgumentParser(description=__doc__,
-	                            formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
 
-	parser.add_argument(
-	        "--fasta",
-	        required=True,
-	        type=str,
-	        metavar='.fa',
-	        help="Input; the path to the ENA formatted genome assembly fasta file. The script does not support compressed files (e.g.: .fa.gz)",
-	    )
+    parser.add_argument(
+        "--fasta",
+        required=True,
+        type=str,
+        metavar=".fa",
+        help="Input; the path to the ENA formatted genome assembly fasta file. The script does not support compressed files (e.g.: .fa.gz)",
+    )
 
-	parser.add_argument(
-	        "--out",
-	        required=False,
-	        type=str,
-	        metavar='.tsv',
-	        help="Output [optional]; the path to save the generated alias file. If not specified, the outfile will be saved to the current directory with using the file name of the fasta file appended with the suffix: _alias.tsv"
-	    )
+    parser.add_argument(
+        "--out",
+        required=False,
+        type=str,
+        metavar=".tsv",
+        help="Output [optional]; the path to save the generated alias file. If not specified, the outfile will be saved to the current directory with using the file name of the fasta file appended with the suffix: _alias.tsv",
+    )
 
-	return parser.parse_args()
+    return parser.parse_args()
 
 
-def make_alias_dictionary(ENA_fasta_file:str) -> dict[str, dict[str, str]]:
-	"""
-	Load the fasta file given by the input argument. Test if it looks like an ENA formatted fasta. 
-	Then split and extract the substrings that contain the GenBank header, and, if present, the contig header. 
-	Returns a nested dictionary with the ENA header as outer key, NCBI header as inner key, 
-	and contig name as inner value.
-	"""
+def make_alias_dictionary(fasta: str) -> dict[str, dict[str, str]]:
+    """
+    Load the fasta file given by the input argument. Test if it looks like an ENA formatted fasta.
+    Then split and extract the substrings that contain the GenBank header, and, if present, the contig header.
+    Returns a nested dictionary with the ENA header as outer key, NCBI header as inner key,
+    and contig name as inner value.
+    """
 
-	alias_dict={}
+    alias_dict = {}
 
-	with open(fasta,'r') as file:
-	    for line in file:
-	        if line.startswith('>'):  											 # Find fasta headers in the multi-fasta
-	        	if line.startswith('>ENA|'):
-	        		ENA_fasta_header=re.split('>| ',line.rstrip())[1]  				 # Split on two delimiters (separated by the vertical bar regexp operator): the > and the first white space in the string
-	        		NCBI_fasta_header=re.split('\\|| ', line.rstrip())[2]  			 # The double-escaped vertical bar - \\| - splits on the vertical bar in the string; the second vertical bar is a regexp operator that states that a second delimiter will follow: in this case the white space
-	        		if re.search('contig: ', line):
-		        		contig_name_fasta_header=re.split('contig: ',line.rstrip())[1] 	 # Splits on the substring 'contig: '
-	        		else:
-	        			contig_name_fasta_header=''
-	        		alias_dict[ENA_fasta_header]={NCBI_fasta_header : contig_name_fasta_header}
-	        	else:
-	        		raise ValueError	
+    with open(fasta, "r") as file:
+        for line in file:
+            if line.startswith(">"):
+                # Find fasta headers in the multi-fasta
+                if line.startswith(">ENA|"):
+                    ENA_fasta_header = re.split(">| ", line.rstrip())[1]
+                    # Split on two delimiters (separated by the vertical bar regexp operator): the > and the first white space in the string
+                    NCBI_fasta_header = re.split("\\|| ", line.rstrip())[2]
+                    # The double-escaped vertical bar - \\| - splits on the vertical bar in the string;
+                    # the second vertical bar is a regexp operator that states that a second delimiter will follow: in this case the white space
+                    if re.search("contig: ", line):
+                        contig_name_fasta_header = re.split("contig: ", line.rstrip())[1]
+                    # Splits on the substring 'contig: '
+                    else:
+                        contig_name_fasta_header = ""
+                    alias_dict[ENA_fasta_header] = {NCBI_fasta_header: contig_name_fasta_header}
+                else:
+                    raise ValueError
 
-	return alias_dict
+    return alias_dict
 
 
 if __name__ == "__main__":
+    args = parse_the_arguments()
 
-	# Parse the arguments:
-	try:
-	    args=parse_the_arguments()
-	except:
-	    sys.exit()
+    fasta = args.fasta
 
-	fasta=args.fasta
+    if args.out is not None:
+        alias_output = args.out
+    else:
+        alias_output = fasta + "_alias.tsv"
 
-	if args.out is not None:
-		alias_output=args.out
-	else:
-		alias_output=fasta+'_alias.tsv'
+    # Read the ENA formated fasta file and generate dictionary with the aliases:
+    try:
+        alias_dict = make_alias_dictionary(fasta)
+    except ValueError as e:
+        print("""
+              ERROR: This does not seem to be an ENA formatted fasta file.
+              Please make sure that the file is not compressed and that the headers start with: ">ENA|".
+              """)
+        raise e
 
-	# Read the ENA formated fasta file and generate dictionary with the aliases:
-	try:
-		alias_dict=make_alias_dictionary(fasta)
-	except ValueError:
-		print('ERROR: This does not seem to be an ENA formatted fasta file.\nPlease make sure that the file is not compressed and that the headers start with: ">ENA|".')
-		sys.exit()
-
-	# Write the alias dictionary to a tab-separated file:
-	with open(alias_output,'w') as file:
-	    for outer_key, outer_value in alias_dict.items():
-	        for inner_key, inner_value in outer_value.items():
-	            aliases = ''.join(f'{outer_key}\t{inner_key}\t{inner_value}\n')
-	            file.write(aliases)
+    # Write the alias dictionary to a tab-separated file:
+    with open(alias_output, "w") as file:
+        for outer_key, outer_value in alias_dict.items():
+            for inner_key, inner_value in outer_value.items():
+                aliases = "".join(f"{outer_key}\t{inner_key}\t{inner_value}\n")
+                file.write(aliases)
