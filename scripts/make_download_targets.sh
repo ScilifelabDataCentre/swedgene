37a7608b433e2e7c58bc97ef026a096c57861d84
Quentin Ågren <quentin@scilifelab.se>
2024-05-23 15:24:26 +0200

    Generate makefile with files to download

---
+++ scripts/make_download_targets.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Generate a makefile defining data files to download
+
+BASE_DIR="$(git rev-parse --show-toplevel)"
+source "$BASE_DIR/scripts/utils.sh"
+CACHE_DIR="$BASE_DIR/.downloads"
+MAKEFILE="$BASE_DIR/targets.mk"
+DOWNLOAD_EXTENSIONS=(".fna" ".gff")
+
+_extract_urls() {
+    yq --no-doc '(.assembly, .tracks[]) | [.url, .fileName // "", fileName] | join(";")' "$@"
+}
+
+_should_download () {
+    for ext in ${DOWNLOAD_EXTENSIONS[@]};
+    do
+	[[ "$1" == *$ext* ]] && return 0
+    done
+    return 1
+}
+
+mkdir -p "$CACHE_DIR"
+
+printf 'define DOWNLOAD_TARGETS\n' > "$MAKEFILE"
+
+while IFS=";" read url target config;
+do
+    dir=${config%/*}
+    if [[ -z "$target" ]];
+    then
+	target=${url##*/}
+    fi
+    target_path="$dir/$(std_extension $target)"
+    _should_download $target_path || continue
+    printf "$target_path\n" | tee -a "$MAKEFILE"
+    if [[ ! -e  "$CACHE_DIR/$target_path" || ! ( "$(< $CACHE_DIR/$target_path)" == $url ) ]];
+    then
+	mkdir -p "$CACHE_DIR/${target_path%/*}"
+	printf "$url" > "$CACHE_DIR/$target_path"
+    fi
+done < <(_extract_urls data/**/config.yml)
+
+printf 'endef\n' >> "$MAKEFILE"
