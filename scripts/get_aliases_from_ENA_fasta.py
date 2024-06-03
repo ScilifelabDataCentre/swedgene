8e4d6c0446c1c4a9aed0a5c3f58061cbe5ff4901
Daniel P Brink <daniel.brink@scilifelab.se>
2024-06-03 10:30:42 +0200

    Added support for (b)gzipped assemblies.

--- scripts/get_aliases_from_ENA_fasta.py
+++ scripts/get_aliases_from_ENA_fasta.py
@@ -1,7 +1,7 @@
 """
-This script generates an JBrowse2-compatible refNameAliases file from an ENA fasta file. The alias file will
-allow for displaying annotation tracks from NCBI GenBank on an ENA formatted assembly without having to modify
-the data files themselves. Additionally, for cases where the original contig name is preserved in the ENA fasta
+This script generates an JBrowse2-compatible refNameAliases file from genome assembly fasta files downloaded from ENA.
+The alias file will allow for displaying annotation tracks from NCBI GenBank on an ENA formatted assembly without having
+to modify the data files themselves. Additionally, for cases where the original contig name is preserved in the ENA fasta
 header, it will also allow for displaying tracks that calls on contig name fasta headers.
 
 Example of a ENA formatted fasta header from a genome assembly:
@@ -13,12 +13,24 @@ NCBI GenBank formatted version of the same assembly:
 Example of the original contig header from the same assembly:
 >scaffold_1
 
-### Usage:
+### Input:
+The path to a fasta file downloaded from ENA. The option --fasta is required.
+The script supports gzipped, bgzipped, and non-compressed ENA fasta files.
+
+### Output:
+A tab-separated file with the following columns:
+ENA header, NCBI header, Contig name
+
+If not specified, the outfile will be saved to the same directory as the fasta file, with the
+file name of the fasta file appended with the extension: .alias
+
+### Examples:
 python get_aliases_from_ENA_fasta.py --fasta filename.fa
 
 """
 
 import argparse
+import gzip
 import re
 
 
@@ -34,7 +46,9 @@ def parse_the_arguments():
         required=True,
         type=str,
         metavar=".fa",
-        help="Input; the path to the ENA formatted genome assembly fasta file. The script does not support compressed files (e.g.: .fa.gz)",
+        help="""
+        Input; the path to the ENA formatted genome assembly fasta file. The script does not support compressed files (e.g.: .fa.gz)
+        """,
     )
 
     parser.add_argument(
@@ -42,7 +56,10 @@ def parse_the_arguments():
         required=False,
         type=str,
         metavar=".tsv",
-        help="Output [optional]; the path to save the generated alias file. If not specified, the outfile will be saved to the current directory with using the file name of the fasta file appended with the suffix: _alias.tsv",
+        help="""
+        Output [optional]; the path to save the generated alias file. If not specified, the outfile will be saved to the current directory
+        with using the file name of the fasta file appended with the suffix: .alias
+        """,
     )
 
     return parser.parse_args()
@@ -50,33 +67,47 @@ def parse_the_arguments():
 
 def make_alias_dictionary(fasta: str) -> dict[str, dict[str, str]]:
     """
-    Load the fasta file given by the input argument. Test if it looks like an ENA formatted fasta.
-    Then split and extract the substrings that contain the GenBank header, and, if present, the contig header.
-    Returns a nested dictionary with the ENA header as outer key, NCBI header as inner key,
-    and contig name as inner value.
+    Load the fasta file given by the input argument. First check if it is gzipped or not.
+    Then call on the subfunction process_fasta_headers. The subfunction tests if it looks like
+    an ENA formatted fasta; if not: exit. If it is ENA formatted: it splits and extracts the substrings
+    that contain the GenBank header, and, if present, the contig header. It returns a nested dictionary
+    with the ENA header as outer key, NCBI header as inner key, and contig name as inner value. This
+    dictionary is then passed back to make_alias_dictionary and returned.
     """
 
     alias_dict = {}
 
-    with open(fasta, "r") as file:
-        for line in file:
-            if line.startswith(">"):
-                # Find fasta headers in the multi-fasta
-                if line.startswith(">ENA|"):
-                    ENA_fasta_header = re.split(">| ", line.rstrip())[1]
-                    # Split on two delimiters (separated by the vertical bar regexp operator): the > and the first white space in the string
-                    NCBI_fasta_header = re.split("\\|| ", line.rstrip())[2]
-                    # The double-escaped vertical bar - \\| - splits on the vertical bar in the string;
-                    # the second vertical bar is a regexp operator that states that a second delimiter will follow: in this case the white space
-                    if re.search("contig: ", line):
-                        contig_name_fasta_header = re.split("contig: ", line.rstrip())[1]
-                    # Splits on the substring 'contig: '
-                    else:
-                        contig_name_fasta_header = ""
-                    alias_dict[ENA_fasta_header] = {NCBI_fasta_header: contig_name_fasta_header}
-                else:
-                    raise ValueError
+    try:
+        with gzip.open(fasta, "rt") as file:
+            alias_dict = process_fasta_headers(file, alias_dict)
+            print(f"The file {fasta} is gzipped. Creating refNameAlias file...")
+    except gzip.BadGzipFile:
+        with open(fasta, "r") as file:
+            alias_dict = process_fasta_headers(file, alias_dict)
+            print(f"The file {fasta} is not gzipped. Creating refNameAlias file...")
 
+    return alias_dict
+
+
+def process_fasta_headers(file: str, alias_dict: dict) -> dict[str, dict[str, str]]:
+    """
+    Subfunction for make_alias_dictionary. Processes the fasta headers and extracts the ENA
+    and NCBI headers, and, if present, the contig header. It raises an error if the fasta headers
+    are not formatted in the ENA style. Returns a nested dictionary back to make_alias_dictionary.
+    """
+    for line in file:
+        if line.startswith(">"):
+            # Find fasta headers in the multi-fasta
+            if line.startswith(">ENA|"):
+                ENA_fasta_header = re.split(">| ", line.rstrip())[1]
+                NCBI_fasta_header = re.split("\\|| ", line.rstrip())[2]
+                if re.search("contig: ", line):
+                    contig_name_fasta_header = re.split("contig: ", line.rstrip())[1]
+                else:
+                    contig_name_fasta_header = ""
+                alias_dict[ENA_fasta_header] = {NCBI_fasta_header: contig_name_fasta_header}
+            else:
+                raise ValueError
     return alias_dict
 
 
@@ -88,7 +119,7 @@ if __name__ == "__main__":
     if args.out is not None:
         alias_output = args.out
     else:
-        alias_output = fasta + "_alias.tsv"
+        alias_output = fasta + ".alias"
 
     # Read the ENA formated fasta file and generate dictionary with the aliases:
     try:
@@ -106,3 +137,5 @@ if __name__ == "__main__":
             for inner_key, inner_value in outer_value.items():
                 aliases = "".join(f"{outer_key}\t{inner_key}\t{inner_value}\n")
                 file.write(aliases)
+
+    print("Wrote alias file to:", alias_output)
