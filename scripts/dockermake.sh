cf385fc52c947bd02e39d6bc054c95ebe4f21bd7
Quentin Ågren <quentin@scilifelab.se>
2024-07-09 09:45:25 +0200

    Create data mount point on host

--- scripts/dockermake.sh
+++ scripts/dockermake.sh
@@ -1,3 +1,4 @@
 # Run `make` in a Docker container
 CWD="$(pwd)"
-docker run -u "$(id -u):$(id -g)" -v "$CWD/${DATA_DIR:-data}:/swedgene/data" -v "$CWD/Makefile:/swedgene/Makefile" swedgene-data make "$@"
+mkdir -p "${DATA_DIR:=data}"
+docker run -u "$(id -u):$(id -g)" -v "$CWD/${DATA_DIR}:/swedgene/data" -v "$CWD/Makefile:/swedgene/Makefile" swedgene-data make "$@"
