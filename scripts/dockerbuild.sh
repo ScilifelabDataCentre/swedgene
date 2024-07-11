41649f8ca2389848e275ec99f40a199e4dd588f4
Quentin Ågren <quentin@scilifelab.se>
2024-07-11 10:54:13 +0200

    Switch to disable user forwarding in local builds

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,13 +1,20 @@
 # Helper script to build Docker images
-
+#
+# By default, the host user ID and group ID are forwarded through
+# build arguments. To disable this behavior and use the dockerfile
+# defaults, set the SWG_DEFAULT_USER environment variable to 1.
 _DEFAULT_TAG=local
 
 if [[ -z "$1" || "$1" == "data" ]];
 then
     _DEFAULT_IMAGE=ghcr.io/scilifelabdatacentre/data-builder
     _DEFAULT_DOCKERFILE=dockerfiles/data.dockerfile
-    SWG_BUILD_ARGS=("--build-arg" "SWG_UID=${SWG_UID:-$(id -u)}" "--build-arg" "SWG_GID=${SWG_GID:-$(id -g)}")
-    docker build "${SWG_BUILD_ARGS[@]}" \
+    declare -a _BUILD_ARGS
+    if [[ -z ${SWG_DEFAULT_USER} ]];then
+	_BUILD_ARGS+=("--build-arg" "SWG_UID=$(id -u)")
+	_BUILD_ARGS+=("--build-arg" "SWG_GID=$(id -g)")
+    fi
+    docker build "${_BUILD_ARGS[@]}" \
 	   -t "${SWG_DOCKER_IMAGE:-$_DEFAULT_IMAGE}:${SWG_DOCKER_TAG:-$_DEFAULT_TAG}" \
 	   -f "${SWG_DOCKERFILE:-$_DEFAULT_DOCKERFILE}" . && exit 0
 fi
