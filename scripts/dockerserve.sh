c10262391224bbea9e79074bb053020430c7c766
Quentin Ågren <quentin@scilifelab.se>
2024-07-12 14:09:24 +0200

    Log message on server start

--- scripts/dockerserve.sh
+++ scripts/dockerserve.sh
@@ -7,7 +7,7 @@
 # SWG_PORT=8000 SWG_TAG=latest ./scripts/dockerserve.sh
 _IMAGE=ghcr.io/scilifelabdatacentre/hugo-site
 _TAG=local
-_NAME=swegene-site
+_NAME=swedgene-site
 _DATA_DIR=data
 _WWW=/usr/share/nginx/html
 _PORT=8080
@@ -18,4 +18,8 @@ docker run -d \
        -p "${SWG_PORT:-$_PORT}":8080 \
        -v "$CWD/${SWG_DATA_DIR:-$_DATA_DIR}":"$_WWW/static/data" \
        --name "${SWG_NAME:-$_NAME}" \
-       "${SWG_IMAGE:-$_IMAGE}:${SWG_TAG:-$_TAG}"
+       "${SWG_IMAGE:-$_IMAGE}:${SWG_TAG:-$_TAG}" && \
+     cat <<EOF
+- Web server is running in container "${SWG_NAME:-$_NAME}".
+- Site can be visited at http://localhost:${SWG_PORT:-$_PORT}
+EOF
