0fa6a8b2beaff1bf144a3142ad09ea2f6e0a8fbd
Quentin Ågren <quentin@scilifelab.se>
2024-07-18 09:57:55 +0200

    Remove _DOCKER in variable names

--- scripts/dockermake.sh
+++ scripts/dockermake.sh
@@ -4,10 +4,10 @@
 # container as a specific user and group. By default, the effective
 # user and group id of the host are used.
 #
-# The environment variables SWG_DOCKER_IMAGE and SWG_DOCKER_TAG can be
+# The environment variables SWG_IMAGE and SWG_TAG can be
 # used to override the Docker image to use. For example:
 #
-# SWG_DOCKER_IMAGE=industrious-squirrel SWG_DOCKER_TAG=slim-buster scripts/dockerbuild.sh build
+# SWG_IMAGE=industrious-squirrel SWG_TAG=slim-buster scripts/dockerbuild.sh build
 
 _DEFAULT_TAG="latest"
 _DEFAULT_IMAGE="ghcr.io/scilifelabdatacentre/swg-data-builder"
@@ -15,7 +15,7 @@ _DEFAULT_IMAGE="ghcr.io/scilifelabdatacentre/swg-data-builder"
 CWD="$(pwd)"
 if [[ -n $SWG_UID || -n $SWG_GID ]];
 then
-    SWG_DOCKER_USER=("-u" "${SWG_UID:-$(id -u)}:${SWG_GID:-$(id -g)}")
+    SWG_USER=("-u" "${SWG_UID:-$(id -u)}:${SWG_GID:-$(id -g)}")
 fi
 
 if [[ "$1" == "--test" || "$1" == "-t" ]]; then
@@ -38,21 +38,21 @@ if [[ "$1" == "--test" || "$1" == "-t" ]]; then
 	   nginx:alpine
 
     # Run the test build
-    docker run --rm "${SWG_DOCKER_USER[@]}" \
+    docker run --rm "${SWG_USER[@]}" \
 	   --network=swg-test-net \
 	   -v "$CWD/$DATA_DIR:/swedgene/data" \
 	   -v "$CWD/Makefile:/swedgene/Makefile" \
 	   -v "$CWD/tests/config:/swedgene/config" \
 	   -v "$CWD/$INSTALL_DIR:/swedgene/hugo/static" \
-	   "${SWG_DOCKER_IMAGE:-$_DEFAULT_IMAGE}:${SWG_DOCKER_TAG:-$_DEFAULT_TAG}" make "$@"
+	   "${SWG_IMAGE:-$_DEFAULT_IMAGE}:${SWG_TAG:-$_DEFAULT_TAG}" make "$@"
 
     docker container rm -f fixtures
     docker network remove swg-test-net
 else
     mkdir -p "${DATA_DIR:=data}" "${INSTALL_DIR:=hugo/static}"
-    docker run --rm "${SWG_DOCKER_USER[@]}" \
+    docker run --rm "${SWG_USER[@]}" \
 	   -v "$CWD/${DATA_DIR}:/swedgene/data" \
 	   -v "$CWD/Makefile:/swedgene/Makefile" \
 	   -v "$CWD/${INSTALL_DIR}:/swedgene/hugo/static" \
-	   "${SWG_DOCKER_IMAGE:-$_DEFAULT_IMAGE}:${SWG_DOCKER_TAG:-$_DEFAULT_TAG}" make "$@"
+	   "${SWG_IMAGE:-$_DEFAULT_IMAGE}:${SWG_TAG:-$_DEFAULT_TAG}" make "$@"
 fi
