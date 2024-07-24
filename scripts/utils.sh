11a761aa66470d9b80d42b1c5dcfd1de851b6269
Quentin Ågren <quentin@scilifelab.se>
2024-07-24 10:06:47 +0200

    Declarative mapping of file extensions to normalize

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,15 +6,26 @@ chrom_names() {
     sed -n 's/>\([A-Za-z0-9._]\+\).*/\1/p' "$1"
 }
 
+_zip_extensions=(.gz .zip)
+
+# Mapping between common extensions used for genomic files, and the
+# normalized equivalent we use internally.
+_fasta=fna
+_gff=gff
+declare -A _bio_extensions=(
+    [fasta]=$_fasta
+    [fa]=$_fasta
+    [gff3]=$_gff
+)
 
 std_extension() {
-    for name in "$@";
-    do
-	case $name in
-	    *.fasta*)
-		echo ${name/.fasta/.fna} ;;
-	    *)
-		echo $name ;;
-	esac
+    filename="$1"
+    for ext in "${!_bio_extensions[@]}"; do
+	if [[ "$filename" =~ \."$ext"(\.|$) ]]; then
+	    _std_ext=${_bio_extensions[$ext]}
+	    filename=${filename/".$ext"/".$_std_ext"}
+	    break
+	fi
     done
+    echo "$filename"
 }
